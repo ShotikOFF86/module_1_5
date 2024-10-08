b3202428f980473ac7be66cfdc6e0421fb7b3f82
ShotikOFF86 <ledidog2000@gmail.com>
2024-10-08 16:02:37 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+from operator import truediv
+
+immutable_var = ("собака",125, "бомба", "питарда", 15.5, False)
+print(immutable_var)
+
+mutable_list = [123.4, 90, "слон", True]
+print(mutable_list)
+mutable_list[2]="змея"
+print(mutable_list)
+
+
